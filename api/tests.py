8190c0d3f5f36db1dd653488ff6d535da1795458
SherSingh <wbsher91@gmail.com>
2016-02-18 12:32:08 +0530

    adding rest apis

---
+++ api/tests.py
@@ -0,0 +1,50 @@
+import requests
+import json
+import datetime
+
+from django.test import TestCase
+from django.test import TestCase
+from django.contrib.auth.models import User
+
+from work.models import *
+
+class RestApiTestCase(TestCase):
+    """
+    test cases for RESTful APIs
+    """
+
+    def test_get_all_projects(self):
+        """
+        test cases to get JSON for all projects
+        """
+        url = "http://localhost:8000/api/projects/"
+        r = requests.get(url)
+        print r.text, r.status_code
+        self.assertEqual(r.status_code, 200)
+
+    def test_get_project_detail(self):
+        """
+        test case to get project detail by primary key
+        """
+        url = "http://localhost:8000/api/projects/1"
+        r = requests.get(url)
+        print r.text, r.status_code
+        self.assertEqual(r.status_code, 200)
+
+    def test_get_all_tasks(self):
+        """
+        test cases to get JSON for all tasks
+        """
+        url = "http://localhost:8000/api/tasks/"
+        r = requests.get(url)
+        print r.text, r.status_code
+        self.assertEqual(r.status_code, 200)
+
+    def test_get_task_detail(self):
+        """
+        test case to get task detail by primary key
+        """
+        url = "http://localhost:8000/api/tasks/1"
+        r = requests.get(url)
+        print r.text, r.status_code
+        self.assertEqual(r.status_code, 200)
---
+++ api/views.py
@@ -0,0 +1,105 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.renderers import JSONRenderer
+from rest_framework.parsers import JSONParser
+from work.models import Project, Task
+from work.serializers import ProjectSerializer, TaskSerializer
+
+class JSONResponse(HttpResponse):
+    """
+    An HttpResponse that renders its content into JSON.
+    """
+    def __init__(self, data, **kwargs):
+        content = JSONRenderer().render(data)
+        kwargs['content_type'] = 'application/json'
+        super(JSONResponse, self).__init__(content, **kwargs)
+
+@csrf_exempt
+def project_list(request):
+    """
+    List all code projects, or create a new project.
+    """
+    if request.method == 'GET':
+        projects = Project.objects.all()
+        serializer = ProjectSerializer(projects, many=True)
+        return JSONResponse(serializer.data)
+
+    elif request.method == 'POST':
+        data = JSONParser().parse(request)
+        serializer = ProjectSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JSONResponse(serializer.data, status=201)
+        return JSONResponse(serializer.errors, status=400)
+
+@csrf_exempt
+def project_detail(request, pk):
+    """
+    Retrieve, update or delete a code project.
+    """
+    try:
+        project = Project.objects.get(pk=pk)
+    except Project.DoesNotExist:
+        return HttpResponse(status=404)
+
+    if request.method == 'GET':
+        serializer = ProjectSerializer(project)
+        return JSONResponse(serializer.data)
+
+    elif request.method == 'PUT':
+        data = JSONParser().parse(request)
+        serializer = ProjectSerializer(project, data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JSONResponse(serializer.data)
+        return JSONResponse(serializer.errors, status=400)
+
+    elif request.method == 'DELETE':
+        project.delete()
+        return HttpResponse(status=204)
+
+@csrf_exempt
+def task_list(request):
+    """
+    List all the tasks, or create a new task.
+    """
+    if request.method == 'GET':
+        tasks = Task.objects.all()
+        serializer = TaskSerializer(tasks, many=True)
+        return JSONResponse(serializer.data)
+
+    elif request.method == 'POST':
+        data = JSONParser().parse(request)
+        serializer = TaskSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JSONResponse(serializer.data, status=201)
+        return JSONResponse(serializer.errors, status=400)
+
+@csrf_exempt
+def task_detail(request, pk):
+    """
+    Retrieve, update or delete a code task.
+    """
+    try:
+        task = Task.objects.get(pk=pk)
+    except Task.DoesNotExist:
+        return HttpResponse(status=404)
+
+    if request.method == 'GET':
+        serializer = TaskSerializer(task)
+        return JSONResponse(serializer.data)
+
+    elif request.method == 'PUT':
+        data = JSONParser().parse(request)
+        serializer = TaskSerializer(task, data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JSONResponse(serializer.data)
+        return JSONResponse(serializer.errors, status=400)
+
+    elif request.method == 'DELETE':
+        task.delete()
+        return HttpResponse(status=204)
+
